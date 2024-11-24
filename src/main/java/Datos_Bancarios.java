615127150f62fa8aa31563dc9bb98230847dab39
Mau <>
2024-11-24 13:57:11 -0600

    Cambio_V0 Con archivos de las cuentas sin arreglar

--- src/main/java/Datos_Bancarios.java
+++ src/main/java/Datos_Bancarios.java
@@ -1,6 +1,5 @@
 package main.java;
 
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
--- src/main/java/Registrarse.java
+++ src/main/java/Registrarse.java
@@ -3,6 +3,7 @@ package main.java;
 
 import tiendaonline.Registrado;
 
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
